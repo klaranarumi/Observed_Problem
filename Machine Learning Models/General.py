9ad22932cf90137bcfa435d4ebc1e92c8ca96c38
Klara Narumi de Hamada Maia <72944672+klaranarumi@users.noreply.github.com>
2023-04-12 23:39:23 -0300

    Update General.py

--- Machine Learning Models/General.py
+++ Machine Learning Models/General.py
@@ -1,9 +1,33 @@
+# Processing
+from sklearn.model_selection import train_test_split
+
+y = df['koi_disposition_f']
+
+X = df.iloc[:,2:43]
+# 70% treino e 30% teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=11)
+
 # Classic Models
 
 ## Linear Regression
 
 ## Logistic Regression
 
-## Tree Decision
+## Naive Bayes
 
-## Forest Decision
+## SVM - Kernel Linear
+
+## SVM - Kernel Sigmoid
+
+## SVM - Kernel Polinomial
+
+## SVM - Kernel RBF
+
+## K-NN
+
+## Decision Tree
+
+## Random Forest
+
+## Gradient Tree Boosting
+
